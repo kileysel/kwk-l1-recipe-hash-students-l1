e983f1c15bf4b27ce9ea55b89b571fc965d05950
kileysel <kiley@selmanhouse.com>
2018-06-06 23:29:12 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -13,15 +13,15 @@
 #  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 ingredients = {
-:almond_flour => "2 cups"
-:gluten_freewhole_oats => "1 cup"
-:kosher_salt => ".5 tea spooon"
-:baking_powder => ".5 tea spoon" 
-:baking_soda => ".25 tea spoon" 
-:xanthan_gum => ".5 tea spoon" 
-:slivered_almonds_lightly_toasted => ".25 cup"
-:mini_dark_chocolate_chips => ".75 cup"
-:olive_oil => ".5 cup"
+:almond_flour => "2 cups",
+:gluten_freewhole_oats => "1 cup",
+:kosher_salt => ".5 tea spooon",
+:baking_powder => ".5 tea spoon", 
+:baking_soda => ".25 tea spoon",
+:xanthan_gum => ".5 tea spoon" ,
+:slivered_almonds_lightly_toasted => ".25 cup",
+:mini_dark_chocolate_chips => ".75 cup",
+:olive_oil => ".5 cup",
 :agave => ".25 cup"
 }
 
